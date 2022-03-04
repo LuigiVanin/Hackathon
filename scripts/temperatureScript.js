35ef01e98dd0d6bb44d892dcdd51ac1edf3855f2
LuigiVanin <luisfvaninmartins@gmail.com>
2022-03-04 20:17:25 -0300

    layout

--- scripts/temperatureScript.js
+++ scripts/temperatureScript.js
@@ -1,23 +1,27 @@
-function getTemperature(lat, lon){
-    const temperature = axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=20bf296004bf9c226850010dd33186c8`);
+function getTemperature(lat, lon) {
+    const temperature = axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=20bf296004bf9c226850010dd33186c8`
+    );
     temperature.then(printInfo);
 }
 
-function printInfo(answer){
+function printInfo(answer) {
     const temperature = (answer.data.main.temp - 273.15).toFixed(1);
 
     const icon = answer.data.weather[0].icon;
     const cityName = answer.data.name;
 
     const putIcon = document.querySelector(".weather");
-    
-    putIcon.innerHTML = `
-    <div>
-    <img src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="">
-    <p>${temperature}°C</p>
-    </div>
-    <p>${cityName}</p>
-    `;
 
+    putIcon.innerHTML = `
+    <div class="image">
+    <img src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="">
     
+    
+    </div>
+    <div class='info'>
+        <h1>${temperature}°C</h1>
+        <p>${cityName}</p>
+    </div>
+    `;
 }
