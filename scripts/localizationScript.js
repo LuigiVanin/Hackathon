1da75f6172b6694d008076bf46824f7848b91ace
pablodamascenoo <pablofacex@gmail.com>
2022-03-04 19:01:20 -0300

    feat: added event listener to button

--- scripts/localizationScript.js
+++ scripts/localizationScript.js
@@ -1,12 +1,17 @@
 let lat = 0
 let long = 0
 
-function GetLocal(){
+let buttonGetLocation = document.querySelector(".loc")
+
+buttonGetLocation.addEventListener("click", getLocal)
+
+function getLocal(){
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position =>{
             lat = position.coords.latitude
             long = position.coords.longitude
+            fillInputs()
         });
     } else { 
         alert("Geolocation is not supported by this browser.");
@@ -14,4 +19,12 @@ function GetLocal(){
 
 }
 
-// GetLocal()
+function fillInputs(){
+
+    let inputs = [...document.querySelectorAll("input")]
+
+    inputs[0].value = lat
+    inputs[1].value = long
+}
+
+getLocal()
