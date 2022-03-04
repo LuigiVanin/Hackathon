428f34e452034927ea8d91e2f21bab2cbd1f179e
pablodamascenoo <pablofacex@gmail.com>
2022-03-04 19:18:31 -0300

    feat: submitGeo function added

--- scripts/localizationScript.js
+++ scripts/localizationScript.js
@@ -2,8 +2,10 @@ let lat = 0
 let long = 0
 
 let buttonGetLocation = document.querySelector(".loc")
+let buttonSubmitLocalization = document.querySelector(".search")
 
 buttonGetLocation.addEventListener("click", getLocal)
+buttonSubmitLocalization.addEventListener("click", submitGeo)
 
 function getLocal(){
 
@@ -25,6 +27,16 @@ function fillInputs(){
 
     inputs[0].value = lat
     inputs[1].value = long
+}
+
+function submitGeo(){
+    
+    let inputs = [...document.querySelectorAll("input")]
+
+    lat = inputs[0].value
+    long = inputs[1].value
+
+    getTemperature(lat, long)
 }
 
 getLocal()
